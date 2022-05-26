31bf951b9550a1a2b6c63711bb28a7ff59fd5164
shankarkasala <umashankarno66@gmail.com>
2022-05-26 21:05:58 +0530

    implimented home page and minor pokedex

--- src/PokeDex.js
+++ src/PokeDex.js
@@ -8,7 +8,9 @@ function PokeDex() {
   const [pokemons, setPokemons] = useState([]);
   const [pokemonDetail, setPokemonDetail] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
-
+  const [query, setQuery] = useState("");
+  const [name, setName] = useState(null);
+  const [number, setNumber] = useState(1);
   const customStyles = {
     content: {
       top: "50%",
@@ -22,13 +24,24 @@ function PokeDex() {
     },
     overlay: { backgroundColor: "grey" },
   };
-
+  useEffect(() => {
+    fetch("https://pokeapi.co/api/v2/pokemon")
+      .then((res) => res.json())
+      .then((json) => setPokemons(json.results))
+      .catch((err) => console.log(err));
+  }, []);
+  useEffect(() => {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${number}/`)
+      .then((res) => res.json())
+      .then((json) => setName(json))
+      .catch((err) => console.log(err));
+  });
   if (!isLoading && pokemons.length === 0) {
     return (
       <div>
         <header className="App-header">
           <h1>Welcome to pokedex !</h1>
-          <h2>Requirement:</h2>
+          {/* <h2>Requirement:</h2>
           <ul>
             <li>
               Call this api:https://pokeapi.co/api/v2/pokemon to get pokedex, and show a list of pokemon name.
@@ -43,7 +56,7 @@ function PokeDex() {
             <li>Implement sorting and pagingation</li>
             <li>Commit your codes after done</li>
             <li>If you do more than expected (E.g redesign the page / create a chat feature at the bottom right). it would be good.</li>
-          </ul>
+          </ul> */}
         </header>
       </div>
     );
@@ -57,13 +70,38 @@ function PokeDex() {
             <div className="App">
               <header className="App-header">
                 <b>Implement loader here</b>
+                {setIsLoading(true)}
+                <ReactLoading height={"20%"} width={"20%"} />
               </header>
             </div>
           </>
         ) : (
           <>
             <h1>Welcome to pokedex !</h1>
+            <input onKeyUp={(e) => setQuery(e.target.value)} />
             <b>Implement Pokedex list here</b>
+            {pokemons &&
+              pokemons
+                .filter((poke) => {
+                  if (query === "") {
+                    return poke.name;
+                  } else if (poke.name.startsWith(query)) {
+                    return poke.name;
+                  }
+                })
+                .map((poke, index) => {
+                  return (
+                    <div
+                      onClick={() => {
+                        setNumber(index + 1);
+                        setPokemonDetail(poke.url);
+                      }}
+                      key={index}
+                    >
+                      {poke.name}
+                    </div>
+                  );
+                })}
           </>
         )}
       </header>
@@ -77,6 +115,21 @@ function PokeDex() {
           style={customStyles}
         >
           <div>
+            {
+              <div>
+                <img src={name.sprites.front_default} />
+                {/* stat name :{" "}
+                {name &&
+                  name.map((st, index) => {
+                    return <div key={index}>{st.stats.stat.name}</div>;
+                  })}
+                base_stat :
+                {name &&
+                  name.map((st, index) => {
+                    return <div key={index}> {st.stats.base_stat}</div>;
+                  })}{" "} */}
+              </div>
+            }
             Requirement:
             <ul>
               <li>show the sprites front_default as the pokemon image</li>
@@ -85,7 +138,10 @@ function PokeDex() {
                 required in tabular format
               </li>
               <li>Create a bar chart based on the stats above</li>
-              <li>Create a  buttton to download the information generated in this modal as pdf. (images and chart must be included)</li>
+              <li>
+                Create a buttton to download the information generated in this
+                modal as pdf. (images and chart must be included)
+              </li>
             </ul>
           </div>
         </Modal>
